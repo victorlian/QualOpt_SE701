35432632f908f6a49333fe81494dda111478f81b
victorlian <vlia679@aucklanduni.ac.nz>
2018-03-17 00:31:54 +1300

    fixed an issue where going back to edit the new password does not go through validation. improved code quality on validation logic.

--- src/main/webapp/app/account/password/confirmPassword.directive.ts
+++ src/main/webapp/app/account/password/confirmPassword.directive.ts
@@ -4,24 +4,45 @@ import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 @Directive({
     selector: '[validateEqual][formControlName],[validateEqual][formControl],[validateEqual][ngModel]',
     providers: [
-        { provide: NG_VALIDATORS, useExisting: forwardRef(() => EqualValidator), multi: true }
+        { 
+            provide: NG_VALIDATORS, 
+            useExisting: forwardRef(() => EqualValidator), 
+            multi: true 
+        }
     ]
 })
-
 export class EqualValidator implements Validator {
-    constructor( @Attribute('validateEqual') public validateEqual: string) {}
+    constructor(@Attribute('validateEqual') public validateEqual: string,
+    @Attribute('editingNew') public editingNew: string) {
+    }
 
-    validate(c: AbstractControl): { [key: string]: any } {
-        // self value (e.g. retype password)
-        let v = c.value;
+    private get isEditingNew() {
+        return this.editingNew === 'true';
+    }
 
-        // control value (e.g. password)
-        let e = c.root.get(this.validateEqual);
+    validate(control: AbstractControl): { [key: string]: any } {
+        // self/other is a pair of new/confirm. When new is self, then other is confirm. Vice versa.
+        let selfValue = control.value;
+        let otherControl = control.root.get(this.validateEqual);
 
-        // value not equal
-        if (e && v !== e.value) return {
-            validateEqual: false
+        // case when editing the confirm field
+        if (otherControl && selfValue !== otherControl.value && !this.isEditingNew) {
+            return {
+                validateEqual: false
+            }
         }
+
+        // case when editing the new field and confirm is the same
+        if (otherControl && selfValue === otherControl.value && this.isEditingNew) {
+            delete otherControl.errors['validateEqual'];
+            if (!Object.keys(otherControl.errors).length) otherControl.setErrors(null);
+        }
+
+        // case when editing the new field and confirm is not the same
+        if (otherControl && selfValue !== otherControl.value && this.isEditingNew) {
+            otherControl.setErrors({ validateEqual: false });
+        }
+
         return null;
     }
 }
